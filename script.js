7ef79f8d1f9249221e1180062bdbb68a2a8a364a
Wefqi <wefqiodeh99@gmail.com>
2023-04-03 15:19:10 -0500

    Linked to database

--- script.js
+++ script.js
@@ -112,3 +112,22 @@ function calculatePosition(element) {
     width: rect.width,
   };
 }
+
+//Database
+
+//Loads database on bottom of webpage after a couple of seconds
+window.onload = function() {
+  const url = "https://cors-anywhere.herokuapp.com/docs.google.com/spreadsheets/d/1-tR6kESj1kzbR8j0AOO_t3mLvbMTynlif-dcqJbvfF0/export?format=csv";
+  const main = document.querySelector("main");
+  //main.innerHTML = "<p>Loading...</p>";
+  fetch(url).then(result=>result.text()).then(function(csvtext) {
+      return csv().fromString(csvtext);
+  }).then(function(csv) {
+      csv.forEach(function(row) {
+          main.innerHTML += "<h3>" + row.Name + "</h3>";
+          main.innerHTML += "<h3>" + row.RoomNumber + "</h3>";
+          main.innerHTML += "<h3>" + row.Email + "</h3>";
+          main.innerHTML += "<h3>" + row.OfficeLocation + "</h3>";
+      })
+  });
+}
