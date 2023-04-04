61ac3c496118258991ffa7f637fa9a2383a82b61
ZacBirgen <isaacrbirgen@lewisu.edu>
2023-04-04 15:43:38 -0500

    Its working... Enough

--- script.js
+++ script.js
@@ -2,74 +2,85 @@ console.clear();
 
 var root  = document.documentElement;
 var body  = document.body;
-var pages = document.querySelectorAll(".page");
+var pages = document.querySelectorAll(".mainFloor");
 var area = document.querySelectorAll(".area");
 var map = document.querySelector(".map");
 var label = document.querySelectorAll(".unselectable");
 //Buttons
-var button = document.querySelectorAll(".button");
 var back = document.querySelector(".back");
+var up = document.querySelector(".up");
+var down = document.querySelector(".down");
 
 for (var i = 0; i < pages.length; i++) {  
-  addListeners(pages[i], area[i], map, back);
+  addListeners(pages[i], area[i], map, back, up, down);
 }
 
-function addListeners(page, area, map, back) {
-  console.log(back)
+function addListeners(page, area, map, back, up, down) {
   area.addEventListener("click", function() {
-    grow(page, area, map, back);
+    grow(page, area, map, back, up, down);
   });
 }
 
-function grow(hero, area, map, back) {
+function grow(hero, area, map, back, up, down) {
     
-    var clone = hero.cloneNode(true);
+  var clone = hero.cloneNode(true);
         
-    var from = calculatePosition(area);
-    var to = calculatePosition(hero);
+  var from = calculatePosition(area);
+  var to = calculatePosition(hero);
   
-    gsap.set(hero, { visibility: "hidden" });
-    gsap.set(clone, { position: "absolute", margin: 0 });
+  gsap.set(hero, { visibility: "hidden" });
+  gsap.set(clone, { position: "absolute", margin: 0 });
     
-    body.appendChild(clone);  
+  body.appendChild(clone);  
         
-    var style = {
-      x: to.left - from.left,
-      y: to.top - from.top,
-      width: to.width,
-      height: to.height,
-      autoRound: false,
-      ease: Power1.easeOut,
-      onComplete: onComplete
-    };
+  var style = {
+    x: to.left - from.left,
+    y: to.top - from.top,
+    width: to.width,
+    height: to.height,
+    autoRound: false,
+    ease: Power1.easeOut,
+    onComplete: onComplete
+  };
      
-    gsap.set(clone, from);  
-    gsap.set(clone, { visibility: "visible" });
-    gsap.to(clone, 0.3, style)
+  gsap.set(clone, from);  
+  gsap.set(clone, { visibility: "visible" });
+  gsap.to(clone, 0.3, style)
       
-    function onComplete() {
-      
-      gsap.set([map, label], { visibility: "hidden" });
-      gsap.set(button, { visibility: "visible" });
-      gsap.set(hero, { visibility: "visible" });
-
-      back.addEventListener("click", function() {
-        shrink(hero, area, map);
-      }); 
-      
-      body.removeChild(clone);
-    }
-  }
-  
-function shrink(hero, area, map) {
+  function onComplete() {
     
+    gsap.set([map, label], { visibility: "hidden" });
+    gsap.set(back, { visibility: "visible" });
+    gsap.set(hero, { visibility: "visible" });
+
+    function backFun() {}
+
+    body.removeChild(clone);
+
+    dirFuns = identifyFloors(findFloors(hero), hero, up, down, back, area, map, backFun);
+    
+    backFun = function() {
+      shrink(hero, area, map, up, down, dirFuns);
+    }
+    back.addEventListener("click", backFun, {once : true}, true); 
+  }
+}
+  
+function shrink(hero, area, map, up, down, dirFuns) {
+  if (dirFuns[0] !== 'null'){
+    up.removeEventListener("click", dirFuns[0], {once : true}, true);
+  }
+  if (dirFuns[1] !== 'null'){
+    down.removeEventListener("click", dirFuns[1], {once : true}, true);
+  }
+
     var clone = hero.cloneNode(true);
         
     var from = calculatePosition(hero);
     var to = calculatePosition(area);
   
     gsap.set(hero, { visibility: "hidden" });
-    gsap.set(button, { visibility: "hidden" });
+    gsap.set([back, up, down], { visibility: "hidden" });
     gsap.set([map, label], { visibility: "visible" });
     gsap.set(clone, { position: "absolute", margin: 0 });
     
@@ -113,6 +124,169 @@ function calculatePosition(element) {
   };
 }
 
+function findFloors(element){
+  var building = element.classList[0];
+  var floors = document.querySelectorAll("." + building);
+  return Array.from(floors);
+}
+
+function identifyFloors(floorList, currFloor, up, down, back, area, map, backFun){
+  var index = floorList.indexOf(currFloor);
+  var upper = true;
+  var lower = true;
+  function upFun() {}
+  upFun = null;
+
+  function downFun() {}
+  downFun = null;
+  
+
+  if (index === -1){
+    console.log("problem");
+  }
+  if (index + 1 >= floorList.length){
+    upper = false;
+  } 
+  if (index - 1 <= -1){
+    lower = false;
+  } 
+
+  if (upper == true && lower == true){
+    gsap.set(up, { visibility: "visible" });
+    gsap.set(down, { visibility: "visible" });
+    
+    upFun = function() {
+      goUp(floorList[index], floorList[index + 1], back, area, map, backFun, downFun);
+    }
+    downFun = function() {
+      goDown(floorList[index], floorList[index - 1], back, area, map, backFun, upFun);
+    }
+    up.addEventListener("click", upFun, {once : true}, true);
+    down.addEventListener("click", downFun, {once : true}, true);
+
+  }else if (upper == true){
+    gsap.set(up, { visibility: "visible" });
+
+    upFun = function() {
+      goUp(floorList[index], floorList[index + 1], back, area, map, backFun, downFun);
+    }
+    up.addEventListener("click", upFun, {once : true}, true);
+
+  }else if (lower == true){
+    gsap.set(down, { visibility: "visible" });
+
+    downFun = function() {
+      goDown(floorList[index], floorList[index - 1], back, area, map, backFun, upFun);
+    }
+    down.addEventListener("click", downFun, {once : true}, true);
+
+  }
+  return [upFun, downFun]
+}
+
+
+
+function goUp(currFloor, upperFloor, back, area, map, backFun, downFun){
+  if (downFun !== 'null'){
+    down.removeEventListener("click", downFun, {once : true}, true);
+  }
+
+  console.log(currFloor);
+  var clone = upperFloor.cloneNode(true);
+        
+  var to = calculatePosition(currFloor);
+  var from = {
+    top: -to.height,
+    left: 0,
+  };
+  
+  gsap.set(currFloor,{ visibility: "hidden" });
+  gsap.set(clone, { position: "absolute", margin: 0 });
+    
+
+  body.appendChild(clone);  
+        
+  var style = {
+    x: 0,
+    y: to.height,
+    autoRound: false,
+    ease: Power1.easeOut,
+    onComplete: onComplete
+  };
+     
+  gsap.set(clone, from);  
+  gsap.set(clone, { visibility: "visible" });
+  gsap.to(clone, 0.3, style)
+      
+  function onComplete() {
+    body.removeChild(clone);
+
+    gsap.set(up,{ visibility: "hidden" });
+    gsap.set(upperFloor, { visibility: "visible" });
+
+    back.removeEventListener("click", backFun, {once : true}, true);
+    
+    function upperFun() {}
+    
+    dirFuns = identifyFloors(findFloors(upperFloor), upperFloor, up, down, back, area, map, upperFun);
+    
+    upperFun = function() {
+      shrink(upperFloor, area, map, up, down, dirfuns);
+    }
+    back.addEventListener("click", upperFun, {once : true}, true); 
+  }
+}
+
+function goDown(currFloor, lowerFloor, back, area, map, backFun, upFun){
+  if (upFun !== 'null'){
+    up.removeEventListener("click", upFun, {once : true}, true);
+  }
+
+  console.log(currFloor);
+  var clone = currFloor.cloneNode(true);
+        
+  var from = calculatePosition(currFloor);
+  
+  gsap.set(currFloor, { visibility: "hidden" });
+  gsap.set(lowerFloor, { visibility: "visible" });
+  gsap.set(clone, { position: "absolute", margin: 0 });
+    
+
+  body.appendChild(clone);  
+        
+  var style = {
+    x: 0,
+    y: -from.height,
+    autoRound: false,
+    ease: Power1.easeOut,
+    onComplete: onComplete
+  };
+     
+  gsap.set(clone, from);  
+  gsap.to(clone, 0.3, style)
+      
+  function onComplete() {
+    
+    body.removeChild(clone);
+    
+    gsap.set(down,{ visibility: "hidden" });
+    back.removeEventListener("click", backFun, {once : true}, true);
+    
+    function lowerFun() {} 
+
+    dirFuns = identifyFloors(findFloors(lowerFloor), lowerFloor, up, down, back, area, map, lowerFun);
+
+    lowerFun = function() {
+      shrink(lowerFloor, area, map, up, down, dirFuns);
+    }
+    back.addEventListener("click", lowerFun, {once : true}, true);
+  }
+}
+
+function wipe(old_thing) {
+  var new_thing = old_thing.cloneNode(true);
+  old_thing.parentNode.replaceChild(new_thing, old_thing);
+}
 
 //Database
 //Loads database on bottom of webpage after a couple of seconds
