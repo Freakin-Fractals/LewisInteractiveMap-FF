457efca3ffc79b464c3d8eaf7d8d15f0cb2986d4
Wefqi <wefqiodeh99@gmail.com>
2023-04-16 14:54:18 -0500

    added array of professor info

--- script.js
+++ script.js
@@ -293,6 +293,10 @@ function goDown(currFloor, lowerFloor, back, area, map, backFun, upFun){
 
 //Database
 //Loads database on bottom of webpage after a couple of seconds
+
+const profInfo = []
+
+console.log(profInfo)
 window.onload = function() {
   const url = "https://cors-anywhere.herokuapp.com/docs.google.com/spreadsheets/d/1-tR6kESj1kzbR8j0AOO_t3mLvbMTynlif-dcqJbvfF0/export?format=csv";
   const main = document.querySelector("main");
@@ -306,6 +310,7 @@ window.onload = function() {
           main.innerHTML += "<h3>" + row.Email + "</h3>";
           main.innerHTML += "<h3>" + row.OfficeLocation + "</h3>";
           main.innerHTML += "---------------------------------"
+          profInfo.push(row)
       })
   });
 }
