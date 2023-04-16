e52dbaf12774c96e9e3341dcddf0c72901359ffe
Wefqi <wefqiodeh99@gmail.com>
2023-04-16 17:26:48 -0500

    created seperate script for db

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.clear();
+//console.clear();
 
 var root  = document.documentElement;
 var body  = document.body;
@@ -289,26 +289,3 @@ function goDown(currFloor, lowerFloor, back, area, map, backFun, upFun){
   }
 }
 
-//Database
-//Loads database on bottom of webpage after a couple of seconds
-
-const profInfo = []
-
-console.log(profInfo)
-window.onload = function() {
-  const url = "https://cors-anywhere.herokuapp.com/docs.google.com/spreadsheets/d/1-tR6kESj1kzbR8j0AOO_t3mLvbMTynlif-dcqJbvfF0/export?format=csv";
-  const main = document.querySelector("main");
-  //main.innerHTML = "<p>Loading...</p>";
-  fetch(url).then(result=>result.text()).then(function(csvtext) {
-      return csv().fromString(csvtext);
-  }).then(function(csv) {
-      csv.forEach(function(row) {
-          main.innerHTML += "<h3>" + row.Name + "</h3>";
-          main.innerHTML += "<h3>" + row.RoomNumber + "</h3>";
-          main.innerHTML += "<h3>" + row.Email + "</h3>";
-          main.innerHTML += "<h3>" + row.OfficeLocation + "</h3>";
-          main.innerHTML += "---------------------------------"
-          profInfo.push(row)
-      })
-  });
-}
